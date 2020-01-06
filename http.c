12a2b5be4bdae8bee4d37af4dc69d914b49a75c9
878Ginger <835896532@qq.com>
2020-01-06 14:23:58 +0800

    添加rio的函数，包括rio_readinitb(rio_t *rp,int fd)，static ssize_t rio_read(rio_t *rp,char *usrbuf,size_t n)，ssize_t rio_writen(int fd,void *usrbuf,size_t n)，ssize_t rio_readlineb(rio_t *rp, void *usrbuf,size_t maxlen)。

--- http.c
+++ http.c
@@ -15,7 +15,80 @@
 #include <errno.h>
 #include <fcntl.h>
 
+void rio_readinitb(rio_t *rp,int fd)
+{
+    rp->rio_fd = fd;
+    rp->rio_cnt = 0;
+    rp->rio_bufptr = rp->rio_buf;
+}
 
+static ssize_t rio_read(rio_t *rp,char *usrbuf,size_t n) 
+{
+    int cnt;
+    while(rp->rio_cnt <= 0){/*Read the file content if buf is empty*/
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,sizeof(rp->rio_buf));
+        if(rp->rio_cnt < 0){
+            if(errno != EINTR){
+                return -1;
+            }
+        }else if(rp->rio_cnt == 0){/*EOF*/
+            return 0;
+        }else {/*reset buf ptr*/
+            rp->rio_bufptr = rp->rio_buf;
+        }
+    }
+    /*when n < rp->rio_cnt, need copy some times */
+    cnt = n;
+    if(rp->rio_cnt < n){/*one time copy end*/
+        cnt = rp->rio_cnt;
+    }
+    memcpy(usrbuf,rp->rio_bufptr,cnt);
+    rp->rio_bufptr += cnt;
+    rp->rio_cnt -= cnt;
+    return cnt;
+}
+
+ssize_t rio_writen(int fd,void *usrbuf,size_t n) 
+{
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = usrbuf;
+ 
+    while(nwritten = write(fd,bufp,nleft) <= 0){
+        if(errno == EINTR){
+            nwritten = 0;
+        }else{
+            return -1;
+        }
+        nleft -= nwritten;
+        bufp += nwritten;
+    }
+    return n;
+}
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf,size_t maxlen)
+{
+    int n,rc;
+    char c,*bufp = usrbuf;
+    for(n = 1; n < maxlen; n++){
+        if (( rc = rio_read(rp,&c,1)) == 1){
+            *bufp++ = c;
+            if(c == '\n'){
+                break;
+            }
+        }else if (rc == 0){
+            if(n == 1){/*EOF no data read*/
+                return 0;
+            }else{/*EOF some data read*/
+                break;
+            }
+        }else{/*ERROR*/
+            return -1;
+        }
+    }
+    *bufp = 0;/*string end sign :'\0'*/
+    return n;
+}
 
 
 void process_trans(int fd);
